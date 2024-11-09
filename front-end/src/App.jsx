37af4c795066b880bec9aeb126772a79151ab37d
turtlelazy <ishraqmahid@gmail.com>
2024-11-09 17:42:39 -0500

    login + registers pages and post listings; make sure to do npm i

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Landing';
 import Post from './pages/Post';
+import PostListing from './pages/PostListings';
+import RegisterPage from './pages/Register';
+import LoginPage from './pages/Login';
 
 function App() {
   return (
@@ -12,6 +15,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Post" element={<Post />} />
+          {/* <Route path="/Post/:id" element={<Post />} /> */}
+          <Route path="/listing" element={<PostListing />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </div>
     </Router>
